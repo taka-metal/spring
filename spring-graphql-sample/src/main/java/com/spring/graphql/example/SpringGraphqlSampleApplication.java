b55e175d86dcec57eebb337cb3e8a637038e38a2
taka-metal <takatai.studyhub@gmail.com>
2024-08-30 12:13:54 +0900

    first commit

---
+++ spring-graphql-sample/src/main/java/com/spring/graphql/example/SpringGraphqlSampleApplication.java
@@ -0,0 +1,15 @@
+package com.spring.graphql.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphqlSampleApplication.class, args);
+		
+		System.out.println("Hello");
+	}
+
+}
